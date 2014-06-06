f79f0d183e86f27a9c7c60457a2c6ca09fa8cb77
vijayajyothi <jyothis.kalangi@gmail.com>
2014-06-06 02:47:57 -0700

    change in helper method for more concise

--- app/helpers/vms_helper.rb
+++ app/helpers/vms_helper.rb
@@ -1,20 +1,20 @@
 module VmsHelper
 
   def vms(dc)
-    vms =  Vm.where(:datacenter=> dc).count
+    vms =  Vm.where('datacenter =? AND ops_status !=?', dc,"Deleted").count
   end
   def cpu(dc)
-    cpus = Vm.where(:datacenter=>dc).collect(&:numcpu).map(&:to_i).sum
+    cpus = Vm.where('datacenter =? AND ops_status !=?', dc,"Deleted").collect(&:numcpu).map(&:to_i).sum
     return cpus
   end
 
   def memory(dc)
-    memories = Vm.where(:datacenter=>dc).collect(&:provisionedspacegb).map(&:to_f).sum
+    memories = Vm.where('datacenter =? AND ops_status !=?', dc,"Deleted").collect(&:provisionedspacegb).map(&:to_f).sum
     return memories
   end
 
   def used_memory(dc)
-    used_memory = Vm.where(:datacenter=>dc).collect(&:usedspacegb).map(&:to_f).sum
+    used_memory = Vm.where('datacenter =? AND ops_status !=?', dc,"Deleted").collect(&:usedspacegb).map(&:to_f).sum
     return used_memory
   end
 end
