d8ce7528df49d9f0d19a2f169ff9129f4a32932d
vijayajyothi <jyothis.kalangi@gmail.com>
2014-06-20 02:07:42 -0700

    importing vdcs with functionalities

--- app/models/vm.rb
+++ app/models/vm.rb
@@ -3,67 +3,91 @@ class Vm < ActiveRecord::Base
   # attr_accessible :application, :boottime, :cluster, :connectionstate, :createdby, :createdtime, :datacenter, :folder, :guestfullname, :gueststate, :hostname, :ipaddress, :memorymb, :numcpu, :persistentid, :powerstate, :provisionedspacegb, :qtynics, :reourcepool, :storagecommitted, :storageuncommitted, :suspendinterval, :suspendtime, :toolsrunningstatus, :toolstatus, :toolsversion, :usedspacegb, :vcserver, :version, :vmhost, :vmname
   
 #ASSOCIATIONS
-  belongs_to :vmhost
-  belongs_to :tier
-  belongs_to :instance
-  belongs_to :folder
-  belongs_to :application
-  belongs_to :vcenter
-  belongs_to :vdc
-  belongs_to :cluster
-  has_many :vdisks
-  has_many :vnics
-  has_many :dns_records
-  has_many :vim_events
-  has_many :f5_nodes
-  has_many :vmvips
-  has_many :f5_vips , :through => :vmvips
+belongs_to :vmhost
+belongs_to :tier
+belongs_to :instance
+belongs_to :folder
+belongs_to :application
+belongs_to :vcenter
+belongs_to :vdc
+belongs_to :cluster
+has_many :vdisks
+has_many :vnics
+has_many :dns_records
+has_many :vim_events
+has_many :f5_nodes
+has_many :vmvips
+has_many :f5_vips , :through => :vmvips
 
 #SEARCHABLES
-  searchable do
-    text :name
-    integer :vmhost_id
-    integer :application_id
-    integer :vcenter_id
-    integer :tier_id
-    integer :instance_id
-    text :status
-    integer :vdc_id
-  end
+searchable do
+  text :name
+  integer :vmhost_id
+  integer :application_id
+  integer :vcenter_id
+  integer :tier_id
+  integer :instance_id
+  text :status
+  integer :vdc_id
+end
 
 
 # CLASS METHODS
 class << self
   def importing_data
     vcenter_data = vcenter_data_import
+    p "vcenter data updated"
+    data_center_data = data_center_data_import
   end
-
-  def vcenter_data_import
-     CSV.foreach("csv_data/powercli/vcenters.csv", :headers => true) do |row|
-      vcenter = Vcenter.find_by_name(row["name"])
-      if vcenter.present?
-        vcenter.ops_status = "Present"
-        vcenter.update_attributes(row.to_hash.slice(*accessible_attributes))
-      else
-        vcenter = Vcenter.new
-        vcenter.ops_status = "New"
-        vcenter.attributes = row.to_hash.slice(*accessible_attributes)
-      end
-       Vcenter.save!
+# vcenter data
+def vcenter_data_import
+  CSV.foreach("csv_data/powercli/vcenters.csv", :headers => true) do |row|
+    vcenter = Vcenter.find_by_name(row["name"])
+    p vcenter
+    if vcenter.present?
+      vcenter.ops_status = "Present"
+      vcenter.update_attributes(row.to_hash.slice(*accessible_attributes))
+    else
+      vcenter = Vcenter.new
+      vcenter.ops_status = "New"
+      vcenter.attributes = row.to_hash.slice(*accessible_attributes)
     end
+    vcenter.save
   end
+end
+
+  # data center vdc data
+  def data_center_data_import
+    CSV.foreach("csv_data/powercli/data_center.csv", :headers => true) do |row|
+
+      vdc = Vdc.find_by_name(row["datacenter"])
+      vcenter = Vcenter.find_by_name(row["vcserver"])
+      if vdc.present?
+        vdc.ops_status = "Present"
+        vdc.update_attributes(row.to_hash.slice(*accessible_attributes))
+      else
+        vdc = Vdc.new
+        vdc.ops_status = "New"
+        vdc.attributes = row.to_hash.slice(*accessible_attributes)
+      end
+      vdc.name = row["datacenter"]
+      vdc.vcenter_id = vcenter.id
+      p vdc
+      vdc.save
+    end
+  end 
 
   def import(file)
-	  ::CSV.foreach(file.path, headers: true) do |row|
-		  vm = find_by_ip(row["ipaddress"])
-		  if vm.present?
-			  vm.ops_status="Present"
-        vm.update_attributes(row.to_hash.slice(*accessible_attributes))
-      else
-        vm = new
-        vm.ops_status="New"
-        vm.attributes = row.to_hash.slice(*accessible_attributes)
-      end
+   ::CSV.foreach(file.path, headers: true) do |row|
+    vm = find_by_ip(row["ipaddress"])
+    if vm.present?
+     vm.ops_status="Present"
+     vm.update_attributes(row.to_hash.slice(*accessible_attributes))
+   else
+    vm = new
+    vm.ops_status="New"
+    vm.attributes = row.to_hash.slice(*accessible_attributes)
+  end
       # vm = find_by_id(row["id"]) || new
       # vm.attributes = row.to_hash.slice(*accessible_attributes)
       vm.save!
