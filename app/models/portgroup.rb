24b332fbd58bc41f66c303213fc8b93f0278c8ef
vijayajyothi <jyothis.kalangi@gmail.com>
2014-06-25 02:19:13 -0700

    changes to pg and ds

--- app/models/portgroup.rb
+++ app/models/portgroup.rb
@@ -1,7 +1,7 @@
 class Portgroup < ActiveRecord::Base
   attr_accessible :a_s, :name, :pnic_id, :vcenter_id, :vdc_id, :vmhost_id, :vswitch, :vswitch_id
 belongs_to :vmhost
-  belongs_to :vswitch
+  # belongs_to :vswitch
   belongs_to :vcenter
   belongs_to :vdc
  # belongs_to :pnic
--- app/models/vm.rb
+++ app/models/vm.rb
@@ -174,7 +174,7 @@ end
 def host_hbas_data
   CSV.foreach("csv_data/powercli/hhbas.csv", :headers => true) do |row|
     vmhost = Vmhost.find_by_name(row["vmhost"])
-    hbas = Hhba.where(:vmhost_id=>vmhost.id, :name=>row["hba"])
+    hbas = Hhba.where(:vmhost_id=>vmhost.id, :name=>row["hba"]).first
     if hbas.present?
       hbas.ops_status = "Present"
       hbas.update_attributes(row.to_hash.slice(*accessible_attributes))
@@ -194,7 +194,59 @@ def host_hbas_data
 
     hbas.save
   end
+end
+
+def port_group_data
+  CSV.foreach("csv_data/powercli/port_group.csv", :headers => true) do |row|
+    vmhost = Vmhost.find_by_name(row["vmhost"])
+    pnic = Pnic.find_by_name(row["nic"])
+    pg = Portgroup.where(:vmhost_id=>vmhost.id, :name=>row["portgroup"], :pnic_id => pnic.id).first
+    if pg.present?
+      pg.ops_status = "Present"
+      pg.update_attributes(row.to_hash.slice(*accessible_attributes))
+    else
+      pg = Portgroup.new
+      pg.ops_status = "New"
+      pg.attributes = row.to_hash.slice(*accessible_attributes)
+    end
+    pg.name = row["port_group"]
+    pg.vmhost_id = vmhost.id
+    # pg.vswitch = row["vswitch"]
+    pg.pnic_id = pnic.id
+    pg.a_s = row["activestandby"]
+
+    pg.save
   end
+end
+
+
+def data_store_data_import
+  CSV.foreach("csv_data/powercli/data_store.csv", :headers => true) do |row|
+    vcenter = Vcenter.find_by_name(row["vcserver"])
+
+    vdc = Vdc.find_by_name(row["data"])
+
+    data_store = Datastore.where(:vcenter_id=>vcenter.id, :vdc_id=>vdc.id, :name => row["name"]).first
+    if data_store.present?
+      data_store.ops_status = "Present"
+      data_store.update_attributes(row.to_hash.slice(*accessible_attributes))
+    else
+      data_store = Datastore.new
+      data_store.ops_status = "New"
+      data_store.attributes = row.to_hash.slice(*accessible_attributes)
+    end
+    data_store.name = row["name"]
+    data_store.ds_id = row["id"]
+    data_store.accessible = row["accessible"]
+    data_store.url = row["url"]
+    data_store.capacity = row["capacity"]
+    data_store.freespace = row["freespace"]
+    data_store.vcenter_id = vcenter.id
+    data_store.vdc_id = vdc.id
+
+    data_store.save
+  end
+end
 
 def import(file)
  ::CSV.foreach(file.path, headers: true) do |row|
