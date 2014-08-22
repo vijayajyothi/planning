0fecc9b1f29e468c67cdbe00951e337985524964
vijayajyothi <jyothis.kalangi@gmail.com>
2014-08-22 04:19:59 -0700

    changes-dev

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,7 @@ Opsgps::Application.configure do
 
    config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
-    :address => "10.128.80.90:3000",
+    :address => "smtp.gmail.com",
     :port => 587,
     :domain => "gmail.com",
     :authentication => :login,
