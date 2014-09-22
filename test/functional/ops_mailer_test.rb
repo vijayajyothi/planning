bc8802cf9272895d93b893bba099fca9e0eac840
vijayajyothi <jyothis.kalangi@gmail.com>
2014-09-21 22:13:16 -0700

    changes to the test db

--- test/functional/ops_mailer_test.rb
+++ test/functional/ops_mailer_test.rb
@@ -1,7 +1,7 @@
-require 'test_helper'
+# require 'test_helper'
 
-class OpsMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+# class OpsMailerTest < ActionMailer::TestCase
+#   # test "the truth" do
+#   #   assert true
+#   # end
+# end
